547507609efcafe6d28fdfeee1252fe3996aa864
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-11 16:16:11 -0600

    feat: Added show page for outpatient clinics

--- spec/features/outpatient_clinics/show_spec.rb
+++ spec/features/outpatient_clinics/show_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe "outpatient clinics show page", type: :feature do
     visit "/outpatientclinics/#{clinic_1.id}"
 
   expect(page).to have_content(clinic_1.name)
-  expect(page).to have_content(clinic_1.city)
-  expect(page).to have_content(clinic_1.rank)
-  expect(page).to have_content(clinic_1.radiology)
-  expect(page).to have_content(clinic_1.pediatrics)
-  expect(page).to have_content(clinic_1.womens_health)
-  expect(page).to have_content(clinic_1.referrals)
-  expect(page).to have_content(clinic_1.clinic_services_provided)
+  expect(page).to have_content("City: #{clinic_1.city}")
+  expect(page).to have_content("Rank: #{clinic_1.rank}")
+  expect(page).to have_content("Radiology Clinic: #{clinic_1.radiology}")
+  expect(page).to have_content("Pediatric Clinic: #{clinic_1.pediatrics}")
+  expect(page).to have_content("Womens Health Clinic: #{clinic_1.womens_health}")
+  expect(page).to have_content("Referrals: #{clinic_1.referrals}")
+  expect(page).to have_content("Total Clinic Services: #{clinic_1.clinic_services_provided}")
 
   end
 end

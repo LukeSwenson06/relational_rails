ca42e8a7751f00ad20a6f9d254a55e2304e37bec
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-11 17:14:31 -0600

    test: created test for provider index

--- spec/features/outpatient_clinics/index_spec.rb
+++ spec/features/outpatient_clinics/index_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe "outpatient clinics index page", type: :feature do
  expect(page).to have_content(clinic_1.name)
  expect(page).to have_content(clinic_2.name)
   end
+
+describe "providers index page", type: :feature
+  it "it can show provider and all attributes" do
+    provider = Provider.create!(  name: "John Smith",
+                                  age: 23,
+                                  doctor: true,
+                                  review_rating: 4
+                                )
+  visit "/provider"
+
+  expect(page).to have_content(provider.name)
+  expect(page).to have_content(provider.age)
+  expect(page).to have_content(provider.doctor)
+  expect(page).to have_content(provider.review_rating)
+
+  end
 end

091dcb0955b5fcfd0b32f858f9512af45ea44351
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-11 18:56:18 -0600

    feat: index page created for providers

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/outpatientclinics', to: 'outpatient_clinics#index'
   get '/outpatientclinics/:id', to: 'outpatient_clinics#show'
+  get '/providers', to: 'providers#index'
 end

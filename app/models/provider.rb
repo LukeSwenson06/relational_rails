d2c205967644a9608b8381848ae0d95cb9549cd0
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-12 13:38:16 -0600

    feat: added validates and belongs to provider model

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,2 +1,8 @@
 class Provider < ApplicationRecord
+  belongs_to :outpatient_clinic
+
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :doctor
+  validates_presence_of :review_rating
 end

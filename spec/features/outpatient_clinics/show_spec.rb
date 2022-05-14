d19aae93edf98d6a1f2d7bd198bc9eddbc3a7a96
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-14 10:49:53 -0600

    test: test for provider count method

--- spec/features/outpatient_clinics/show_spec.rb
+++ spec/features/outpatient_clinics/show_spec.rb
@@ -12,6 +12,19 @@ RSpec.describe "outpatient clinics show page", type: :feature do
       clinic_services_provided: 16
     )
 
+    @provider_1 = @clinic_1.providers.create!(
+                                        name: "John Smith",
+                                        age: 23,
+                                        doctor: true,
+                                        review_rating: 4
+                                )
+    @provider_2 = @clinic_1.providers.create!(
+                                        name: "Jane Doe",
+                                        age: 27,
+                                        doctor: true,
+                                        review_rating: 5
+                                )
+
   end
   it "can see the outpatient clinics attributes" do
 
@@ -25,6 +38,13 @@ RSpec.describe "outpatient clinics show page", type: :feature do
   expect(page).to have_content("Womens Health Clinic: #{@clinic_1.womens_health}")
   expect(page).to have_content("Referrals: #{@clinic_1.referrals}")
   expect(page).to have_content("Total Clinic Services: #{@clinic_1.clinic_services_provided}")
+
+  end
+
+  it "can see the outpatient clinics and the number of providers associated with it" do
+    visit "/outpatientclinics/#{@clinic_1.id}"
+
+    expect(page).to have_content("Number of Providers: 2")
 
   end
 end

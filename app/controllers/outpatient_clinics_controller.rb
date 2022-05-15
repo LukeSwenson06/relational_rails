dfed2563e75a2c51eaefff8eaaa6bc1464ec49a1
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-15 17:21:18 -0600

    feat: create new form page created for outpatient clinics

--- app/controllers/outpatient_clinics_controller.rb
+++ app/controllers/outpatient_clinics_controller.rb
@@ -10,4 +10,18 @@ class OutpatientClinicsController < ApplicationController
   def new
 
   end
+
+  def create
+    clinic = OutpatientClinic.create!(
+      name: params[:name],
+      city: params[:city],
+      rank: params[:rank],
+      radiology: params[:radiology],
+      pediatrics: params[:pediatrics],
+      womens_health: params[:womens_health],
+      referrals: params[:referrals],
+      clinic_services_provided: params[:clinic_services_provided])
+      clinic.save
+      redirect_to "/outpatientclinics"
+  end
 end

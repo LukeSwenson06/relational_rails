671b381727152d1d50f2cb5e5fefbd7a356e28b3
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-15 17:20:39 -0600

    test: edited tests

--- spec/features/outpatient_clinics/new_spec.rb
+++ spec/features/outpatient_clinics/new_spec.rb
@@ -1,24 +1,32 @@
 require 'rails_helper'
 
-# User Story 11, Parent Creation
-#
-# As a visitor
-# When I visit the Parent Index page
-# Then I see a link to create a new Parent record, "New Parent"
-# When I click this link
-# Then I am taken to '/parents/new' where I  see a form for a new parent record
-# When I fill out the form with a new parent's attributes:
-# And I click the button "Create Parent" to submit the form
-# Then a `POST` request is sent to the '/parents' route,
-# a new parent record is created,
-# and I am redirected to the Parent Index page where I see the new Parent displayed.
-
 RSpec.describe "Creating new Outpatient Clinics" do
   it "goes to the new page in the outpatient clinics index page " do
     visit '/outpatientclinics'
 
     click_link('New Outpatient Clinic')
     expect(current_path).to eq("/outpatientclinics/new")
+
+  end
+
+  it "can create a new outpatientclinic" do
+    visit '/outpatientclinics/new'
+
+    fill_in('name', with: "Mayo Clinic")
+    fill_in('city', with: "Santa Fe")
+    fill_in('rank', with: 3)
+    fill_in('radiology', with: true)
+    fill_in('pediatrics', with: true)
+    fill_in('womens_health', with: true)
+    fill_in('referrals', with: true)
+    fill_in('clinic_services_provided', with: 25)
+
+    click_button("Create Outpatient Clinic")
+
+
+    expect(current_path).to eq("/outpatientclinics")
+    expect(page).to have_content("Mayo Clinic")
+
 
   end
 end

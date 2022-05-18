5b5854bcaaf3234e0d622da0a652c259860e4a7d
Luke Swenson <LukeSwenson95@outlook.com>
2022-05-18 08:01:43 -0600

    Test: Created test for delete link

--- spec/features/outpatient_clinics/index_spec.rb
+++ spec/features/outpatient_clinics/index_spec.rb
@@ -63,7 +63,7 @@ RSpec.describe "outpatient clinics index page", type: :feature do
   expect(current_path).to eq("/providers")
   end
 
-  it "it can take you to the page" do
+  it "it can take you to the edit page" do
     Provider.destroy_all
     OutpatientClinic.destroy_all
 
@@ -81,5 +81,43 @@ RSpec.describe "outpatient clinics index page", type: :feature do
     click_link("Edit #{clinic_1.name}")
 
     expect(current_path).to eq("/outpatientclinics/#{clinic_1.id}/edit")
+  end
+
+  it "it will delete the Outpatient Clinic" do
+    Provider.destroy_all
+    OutpatientClinic.destroy_all
+
+    clinic_1 = OutpatientClinic.create!(
+      name: "Loveless",
+      city: "Albuquerque",
+      rank: 25,
+      radiology: true,
+      pediatrics: true,
+      womens_health: true,
+      referrals: false,
+      clinic_services_provided: 16
+    )
+
+    clinic_2 = OutpatientClinic.create!(
+      name: "Space Station Health",
+      city: "Space",
+      rank: 80000,
+      radiology: false,
+      pediatrics: false,
+      womens_health: false,
+      referrals: false,
+      clinic_services_provided: 1
+    )
+
+    visit '/outpatientclinics'
+    click_link("Delete #{clinic_1.name}")
+
+    expect(current_path).to eq("/outpatientclinics")
+
+    expect(page).to_not have_content("Loveless")
+    expect(page).to_not have_content(25)
+    expect(page).to_not have_content(16)
+    expect(page).to have_content("Space Station Health")
+    expect(page).to have_content(1)
   end
 end
